8bc56a8544d6415b4285c5ebea3142956294cb60
Ariel Vernaza <avernaza@ubiqua.me>
2017-04-29 16:27:00 -0500

    Play control working

--- Assets/UnityChan/Scripts/UnityChanControlScriptWithRgidBody.cs
+++ Assets/UnityChan/Scripts/UnityChanControlScriptWithRgidBody.cs
@@ -28,7 +28,7 @@ public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 	// 旋回速度
 	public float rotateSpeed = 2.0f;
 	// ジャンプ威力
-	public float jumpPower = 3.0f; 
+	public float jumpPower = 3.0f;
 	// キャラクターコントローラ（カプセルコライダ）の参照
 	private CapsuleCollider col;
 	private Rigidbody rb;
@@ -37,12 +37,12 @@ public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 	// CapsuleColliderで設定されているコライダのHeiht、Centerの初期値を収める変数
 	private float orgColHight;
 	private Vector3 orgVectColCenter;
-	
+
 	private Animator anim;							// キャラにアタッチされるアニメーターへの参照
 	private AnimatorStateInfo currentBaseState;			// base layerで使われる、アニメーターの現在の状態の参照
 
 	private GameObject cameraObject;	// メインカメラへの参照
-		
+
 // アニメーター各ステートへの参照
 	static int idleState = Animator.StringToHash("Base Layer.Idle");
 	static int locoState = Animator.StringToHash("Base Layer.Locomotion");
@@ -63,8 +63,8 @@ public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 		orgColHight = col.height;
 		orgVectColCenter = col.center;
 }
-	
-	
+
+
 // 以下、メイン処理.リジッドボディと絡めるので、FixedUpdate内で処理を行う.
 	void FixedUpdate ()
 	{
@@ -75,9 +75,9 @@ public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 		anim.speed = animSpeed;								// Animatorのモーション再生速度に animSpeedを設定する
 		currentBaseState = anim.GetCurrentAnimatorStateInfo(0);	// 参照用のステート変数にBase Layer (0)の現在のステートを設定する
 		rb.useGravity = true;//ジャンプ中に重力を切るので、それ以外は重力の影響を受けるようにする
-		
-		
-		
+
+
+
 		// 以下、キャラクターの移動処理
 		velocity = new Vector3(0, 0, v);		// 上下のキー入力からZ軸方向の移動量を取得
 		// キャラクターのローカル空間での方向に変換
@@ -88,7 +88,7 @@ public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 		} else if (v < -0.1) {
 			velocity *= backwardSpeed;	// 移動速度を掛ける
 		}
-		
+
 		if (Input.GetButtonDown("Jump")) {	// スペースキーを入力したら
 
 			//アニメーションのステートがLocomotionの最中のみジャンプできる
@@ -101,14 +101,14 @@ public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 				}
 			}
 		}
-		
+
 
 		// 上下のキー入力でキャラクターを移動させる
 		transform.localPosition += velocity * Time.fixedDeltaTime;
 
 		// 左右のキー入力でキャラクタをY軸で旋回させる
-		transform.Rotate(0, h * rotateSpeed, 0);	
-	
+		transform.Rotate(0, h * rotateSpeed, 0);
+
 
 		// 以下、Animatorの各ステート中での処理
 		// Locomotion中
@@ -127,17 +127,17 @@ public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 			// ステートがトランジション中でない場合
 			if(!anim.IsInTransition(0))
 			{
-				
+
 				// 以下、カーブ調整をする場合の処理
 				if(useCurves){
 					// 以下JUMP00アニメーションについているカーブJumpHeightとGravityControl
 					// JumpHeight:JUMP00でのジャンプの高さ（0〜1）
 					// GravityControl:1⇒ジャンプ中（重力無効）、0⇒重力有効
 					float jumpHeight = anim.GetFloat("JumpHeight");
-					float gravityControl = anim.GetFloat("GravityControl"); 
+					float gravityControl = anim.GetFloat("GravityControl");
 					if(gravityControl > 0)
 						rb.useGravity = false;	//ジャンプ中の重力の影響を切る
-										
+
 					// レイキャストをキャラクターのセンターから落とす
 					Ray ray = new Ray(transform.position + Vector3.up, -Vector3.up);
 					RaycastHit hitInfo = new RaycastHit();
@@ -151,12 +151,12 @@ public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 							col.center = new Vector3(0, adjCenterY, 0);	// 調整されたコライダーのセンター
 						}
 						else{
-							// 閾値よりも低い時には初期値に戻す（念のため）					
+							// 閾値よりも低い時には初期値に戻す（念のため）
 							resetCollider();
 						}
 					}
 				}
-				// Jump bool値をリセットする（ループしないようにする）				
+				// Jump bool値をリセットする（ループしないようにする）
 				anim.SetBool("Jump", false);
 			}
 		}
