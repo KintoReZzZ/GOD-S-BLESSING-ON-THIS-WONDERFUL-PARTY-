da36f0f25f2c2f6da07fc177ea5e43d758de7c81
Ariel Vernaza <avernaza@ubiqua.me>
2017-04-30 04:57:40 -0500

    For now food is tuna

---
+++ Assets/clone_tuna.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clone_tuna : MonoBehaviour {
+	public GameObject tuna;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float value = Random.value;
+		if (  value > 100.0f && value < 300.0f){
+			Instantiate(tuna, transform.position, transform.rotation);
+		}
+	}
+}
