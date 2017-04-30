88e01b440f357af01f63b3a1ef531485ab526fd1
Ariel Vernaza <avernaza@ubiqua.me>
2017-04-30 01:48:57 -0500

    Basic Bear logic

---
+++ Assets/enemy_detect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_detect : MonoBehaviour {
+
+	public GameObject player;
+	public float minDist = Mathf.Infinity;
+	Animator anim;
+	Vector3 lastPosition = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+  float move = (transform.position - lastPosition).magnitude;
+    Vector3 currentPos = transform.position;
+		float dist = Vector3.Distance(player.transform.position, currentPos);
+		if (dist < minDist)
+			 {
+				transform.LookAt(player.transform);
+				transform.position += transform.forward*2.0f*Time.deltaTime;
+			 }
+		anim.SetFloat("speed", 1);
+	}
+}
--- Assets/follow_me.cs
+++ Assets/follow_me.cs
@@ -12,7 +12,12 @@ public class follow_me : MonoBehaviour {
 	}
 
 	void Update(){
-		transform.position = Vector3.Lerp(transform.position , player.transform.position, Time.deltaTime * 2.0f);
-		transform.rotation = player.transform.rotation;
+		Vector3 currentPos = transform.position;
+		float dist = Vector3.Distance(player.transform.position, currentPos);
+		if (dist > 1.5)
+		{
+			transform.position = Vector3.Lerp(transform.position , player.transform.position, Time.deltaTime * 2.0f);
+			transform.rotation = player.transform.rotation;
+	  }
 	}
 }
