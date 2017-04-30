1f2f406c813094d16c5d80530713f8906f607ecd
Ariel Vernaza <avernaza@ubiqua.me>
2017-04-30 06:13:10 -0500

    Adding moving tuna

--- Assets/clone_tuna.cs
+++ Assets/clone_tuna.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class clone_tuna : MonoBehaviour {
 	public GameObject tuna;
+	Vector3 pos;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +13,12 @@ public class clone_tuna : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float value = Random.value;
-		if (  value > 100.0f && value < 300.0f){
-			Instantiate(tuna, transform.position, transform.rotation);
+		float value = Random.value * 100.0f;
+		if (  value > 50.0f && value < 52.0f){
+			pos = transform.position;
+      pos.x = pos.x + Random.value * 5;
+			pos.z = pos.z + Random.value * 5;
+			Instantiate(tuna, pos, transform.rotation);
 		}
 	}
 }
---
+++ Assets/tuna_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tuna_controller : MonoBehaviour {
+	GameObject[] tuna_path;
+	int tuna_index;
+
+	// Use this for initialization
+	void Start () {
+		tuna_path = GameObject.FindGameObjectsWithTag("Point");
+		tuna_index=0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.LookAt(tuna_path[tuna_index].transform);
+
+		transform.position = Vector3.Lerp(transform.position , tuna_path[tuna_index].transform.position, Time.deltaTime*(Random.value)* 0.02f);
+		Vector3 currentPos = transform.position;
+		float dist = Vector3.Distance(tuna_path[tuna_index].transform.position, currentPos);
+		if(dist < 0.5 ){
+			tuna_index++;
+		}
+		if( tuna_index > tuna_path.Length -1){
+			tuna_index=0;
+		}
+	}
+}
