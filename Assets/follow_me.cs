d653db316e30c1def94ed73c70e4821b0d58a785
Ariel Vernaza <avernaza@ubiqua.me>
2017-04-30 03:06:53 -0500

    friend without state machibe

--- Assets/follow_me.cs
+++ Assets/follow_me.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class follow_me : MonoBehaviour {
 
 	public GameObject player;
+	public AnimationClip run_clip;
+	public AnimationClip attack_clip;
+	public AnimationClip kill_clip;
+	public AnimationClip idle_clip;
+	public AnimationClip dead_clip;
+	Animation anim;
+
 
 	// Use this for initialization
 	void Start () {
+		anim = GetComponent<Animation> ();
+		 anim.AddClip(run_clip, "run");
+		 anim.AddClip(attack_clip, "attack");
+		 anim.AddClip(kill_clip, "kill");
+		 anim.AddClip(idle_clip, "idle");
+		 anim.AddClip(dead_clip, "dead");
 
 	}
 
@@ -18,6 +31,10 @@ public class follow_me : MonoBehaviour {
 		{
 			transform.position = Vector3.Lerp(transform.position , player.transform.position, Time.deltaTime * 2.0f);
 			transform.rotation = player.transform.rotation;
+			anim.Play ("run");
 	  }
+		else{
+			anim.Play ("idle");
+		}
 	}
 }
