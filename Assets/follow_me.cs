a87625d335c7a0a509f463918d64025f7fad09e9
Ariel Vernaza <avernaza@ubiqua.me>
2017-04-30 03:28:13 -0500

    Adding action

--- Assets/follow_me.cs
+++ Assets/follow_me.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class follow_me : MonoBehaviour {
 
 	public GameObject player;
+	GameObject enemy;
 	public AnimationClip run_clip;
 	public AnimationClip attack_clip;
 	public AnimationClip kill_clip;
@@ -24,17 +25,55 @@ public class follow_me : MonoBehaviour {
 
 	}
 
+	GameObject FindClosestEnemy()
+{
+    GameObject[] gos;
+    gos = GameObject.FindGameObjectsWithTag("Enemy");
+    GameObject closest = null;
+    float distance = Mathf.Infinity;
+    Vector3 position = transform.position;
+    foreach (GameObject go in gos)
+    {
+        Vector3 diff = go.transform.position - position;
+        float curDistance = diff.sqrMagnitude;
+        if (curDistance < distance)
+        {
+            closest = go;
+            distance = curDistance;
+        }
+    }
+    return closest;
+}
+
 	void Update(){
 		Vector3 currentPos = transform.position;
-		float dist = Vector3.Distance(player.transform.position, currentPos);
-		if (dist > 1.5)
-		{
-			transform.position = Vector3.Lerp(transform.position , player.transform.position, Time.deltaTime * 2.0f);
-			transform.rotation = player.transform.rotation;
-			anim.Play ("run");
-	  }
-		else{
-			anim.Play ("idle");
+		float ally_dist = Vector3.Distance(player.transform.position, currentPos);
+		float enemy_distance = 0.0f;
+		float ally_enemy_distance = 0.0f;
+    /* Search gameobject by tag enemy */
+     enemy = FindClosestEnemy();
+		 enemy_distance = Vector3.Distance(enemy.transform.position, currentPos);
+		 ally_enemy_distance = Vector3.Distance(enemy.transform.position, player.transform.position);
+		 if (enemy_distance < 10 && ally_enemy_distance < 10)
+		 {
+			transform.position = Vector3.Lerp(transform.position , enemy.transform.position, Time.deltaTime * 1.0f);
+			transform.LookAt(enemy.transform);
+			if(enemy_distance < 2){
+        anim.Play ("attack");
+			}else{
+				anim.Play ("run");
+			}
+		 }else{
+		/*Attack enemy end*/
+				if (ally_dist > 1.5)
+				{
+					transform.position = Vector3.Lerp(transform.position , player.transform.position, Time.deltaTime * 2.0f);
+					transform.rotation = player.transform.rotation;
+					anim.Play ("run");
+	  		}
+					else{
+						anim.Play ("idle");
+					}
 		}
 	}
 }
