d58f9fca5c3e6feeb25d9c81718cece314f70540
Ariel Vernaza <avernaza@ubiqua.me>
2017-04-30 02:48:35 -0500

    when bear animated attack

--- Assets/enemy_detect.cs
+++ Assets/enemy_detect.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class enemy_detect : MonoBehaviour {
 
 	public GameObject player;
+	public AnimationClip run_clip;
+	public AnimationClip attack_clip;
+	public AnimationClip kill_clip;
+	public AnimationClip idle_clip;
+	public AnimationClip dead_clip;
+
+	Animation anim;
 	public float minDist = Mathf.Infinity;
-	Animator anim;
 	Vector3 lastPosition = Vector3.zero;
+	int kill_attack_counter=0;
 
 	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animator>();
+	 anim = GetComponent<Animation> ();
+	  anim.AddClip(run_clip, "run");
+		anim.AddClip(attack_clip, "attack");
+		anim.AddClip(kill_clip, "kill");
+		anim.AddClip(idle_clip, "idle");
+		anim.AddClip(dead_clip, "dead");
 	}
 
 	// Update is called once per frame
@@ -22,8 +34,24 @@ public class enemy_detect : MonoBehaviour {
 		if (dist < minDist)
 			 {
 				transform.LookAt(player.transform);
-				transform.position += transform.forward*2.0f*Time.deltaTime;
-			 }
-		anim.SetFloat("speed", 1);
+			  if(dist > 3)
+				{
+					transform.position += transform.forward*2.0f*Time.deltaTime;
+					anim.Play ("run");
+			 	}else{
+					kill_attack_counter++;
+					if (kill_attack_counter > 150){
+						anim.Play ("kill");
+						if (kill_attack_counter==200)
+								kill_attack_counter =0; // Reset Kill attack
+					}
+					else{
+						anim.Play ("attack");
+					}
+				}
+			}
+			else{
+					anim.Play ("idle");
+			}
 	}
 }
